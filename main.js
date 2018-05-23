edf91da3cc4026e6e3210e1a4381f8be000288ae
Muhammad Kamal <mkamalkayani@gmail.com>
2018-05-23 02:05:06 +0200

    Sort functionality of events working. Event sorted by Id and alphabatical order of title.

--- main.js
+++ main.js
@@ -19,6 +19,40 @@ function getProjectData(){
 	})
 }
 
+// sort by name
+function sortAlphabetically(Data, parameter){
+	Data.sort(function(a, b) {
+		  var nameA = a[parameter].toUpperCase(); // ignore upper and lowercase
+		  var nameB = b[parameter].toUpperCase(); // ignore upper and lowercase
+		  if (nameA < nameB) {
+		  	return -1;
+		  }
+		  if (nameA > nameB) {
+		  	return 1;
+		  }
+
+		  // names must be equal
+		  return 0;
+		});
+	// console.log(Data);
+}
+
+//
+function sortNumerically(Data, parameter){
+	Data.sort(function(a, b) {
+	  if (a[parameter] < b[parameter]) {
+	  	return -1;
+	  }
+	  if (a[parameter] > b[parameter]) {
+	  	return 1;
+	  }
+
+	  // names must be equal
+	  return 0;
+	});
+	console.log(Data);
+}
+
 var projectData = getProjectData();
 var eventCatalogue = document.querySelector('#event-catalogue .card-body');
 
@@ -27,13 +61,80 @@ fetch('API/eventData.json')
 	return eventData.json();
 })
 .then(function(eventData) {
-	// console.log(eventData);
+	placeEvents(eventData);	
+	return eventData;
+})
+.then(function(eventData){
+	
+	// Sort alphabatically by title
+	$('.sort > .alphabatically').on('click', function(e){
+		$(this).parent().find('.active').removeClass('active');
+		$(this).addClass('active');
 
+		eventCatalogue.innerHTML = "";
+		sortAlphabetically(eventData, 'title');
+		placeEvents(eventData);
+	});
+
+	// Sort by event ID
+	$('.sort > .event-id').on('click', function(e){
+		$(this).parent().find('.active').removeClass('active');
+		$(this).addClass('active');
+
+		eventCatalogue.innerHTML = "";
+		sortNumerically(eventData, 'id');
+		placeEvents(eventData);
+	})
+})
+.then(function(){
+	$('#event-catalogue .card-header').on('click', function(e) {
+		// console.log(e);
+		$(this).find('[class*="angle"]').toggleClass('fa-angle-left fa-angle-down')
+	});
+});
+
+
+// Modal
+// Creating the moal behavior on Project list
+function createProjectModal(id, title, shortDescription, description){
+	var projectModal = `<button type="button" class="btn btn-primary ml-4 my-2 d-block text-truncate" data-toggle="modal" data-target="#projectid${id}">
+	${title}
+	</button>
+
+	<div class="modal fade" id="projectid${id}" tabindex="-1" role="dialog" aria-labelledby="projectModalLongTitle" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+	<div class="modal-content">
+	<div class="modal-header">
+	<h5 class="modal-title" id="projectModalLongTitle">Project Title: ${title}</h5>
+	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	<span aria-hidden="true">&times;</span>
+	</button>
+	</div>
+	<div class="modal-body text-justify">
+	<p><strong>Short Description: </strong>${shortDescription}</p>
+	<p><strong>Description: </strong>${description}</p>
+	</div>
+	<div class="modal-footer">
+	<button type="button" class="btn btn-primary">Like</button>
+	<button type="button" class="btn btn-primary">Comment</button>
+	<button type="button" class="btn btn-primary mr-auto">Join</button>
+	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	<button type="button" class="btn btn-primary">Save changes</button>
+	</div>
+	</div>
+	</div>
+	</div>`
+
+	return projectModal;
+}
+
+
+function placeEvents(eventData){
 	for(let i=0; i<eventData.length; i++){
 		var event = `		
 		<div class="card-header mb-1 d-flex" data-toggle="collapse" data-target="#id${eventData[i].id}">
-			<h6>${eventData[i].title}</h6>
-			<i class="fas fa-angle-left ml-auto"></i>
+		<h6>${eventData[i].title}</h6>
+		<i class="fas fa-angle-left ml-auto"></i>
 		</div>
 		<div id="id${eventData[i].id}" class="event-body collapse"></div>
 		`;
@@ -50,47 +151,6 @@ fetch('API/eventData.json')
 			}	
 		}
 	}
-})
-.then(function(){
-	$('#event-catalogue .card-header').on('click', function(e) {
-		// console.log(e);
-    $(this).find('[class*="angle"]').toggleClass('fa-angle-left fa-angle-down')
-});
-});
-
- 
-// Modal
-// Creating the moal behavior on Project list
-function createProjectModal(id, title, shortDescription, description){
-    var projectModal = `<button type="button" class="btn btn-primary ml-4 my-2 d-block text-truncate" data-toggle="modal" data-target="#projectid${id}">
-      ${title}
-    </button>
-
-    <div class="modal fade" id="projectid${id}" tabindex="-1" role="dialog" aria-labelledby="projectModalLongTitle" aria-hidden="true">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="projectModalLongTitle">Project Title: ${title}</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body text-justify">
-            <p><strong>Short Description: </strong>${shortDescription}</p>
-            <p><strong>Description: </strong>${description}</p>
-          </div>
-          <div class="modal-footer">
-          <button type="button" class="btn btn-primary">Like</button>
-          <button type="button" class="btn btn-primary">Comment</button>
-          <button type="button" class="btn btn-primary mr-auto">Join</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>`
-
-    return projectModal;
 }
 
 
